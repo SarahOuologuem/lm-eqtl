f4bdbfc26c5cc2adc8907e85732d17c8b3e71461
Sonja Stockhaus <stockhaus@cip.ifi.lmu.de>
2023-12-29 11:51:19 +0100

    adapt main.py

--- model/main.py
+++ model/main.py
@@ -15,8 +15,8 @@ import pysam
 import torch
 from torch.utils.data import DataLoader, Dataset
 
-# from tqdm.notebook import tqdm
 from tqdm import tqdm
+from itertools import chain
 
 from encoding_utils import sequence_encoders, sequence_utils
 
@@ -25,11 +25,13 @@ import helpers.misc as misc  # miscellaneous functions
 from helpers.metrics import MaskedAccuracy
 from helpers.temperature_scaling import ModelWithTemperature
 
-
 from models.spec_dss import DSSResNet, DSSResNetEmb, SpecAdd
 
-
 import argparse
+
+################################
+### input parameter handling ###
+################################
 
 parser = argparse.ArgumentParser("main.py")
 
@@ -135,6 +137,52 @@ for param_name in [
     else:
         print(f"{param_name.upper()}: {input_params[param_name]}")
 
+################################
+### read and prepare dataset ###
+################################
+
+if input_params.dataset.endswith(".fa"):
+    seq_df = pd.read_csv(input_params.dataset + ".fai", header=None, sep="\t", usecols=[0], names=["seq_name"])
+elif input_params.dataset.endswith(".parquet"):
+    seq_df = pd.read_parquet(input_params.dataset).reset_index()
+
+seq_df[["split", "sample_id", "seg_name"]] = seq_df["seq_name"].str.split(":", expand=True)
+
+if not input_params.agnostic:
+    # for segment-aware model, assign a label to each segment
+    seg_name = seq_df.seq_name.apply(lambda x: ":".join(x.split(":")[2:]))
+    segment_encoding = seg_name.drop_duplicates().reset_index(drop=True)
+    segment_encoding = {seg_name: idx for idx, seg_name in segment_encoding.items()}
+    seq_df["seg_label"] = seg_name.map(segment_encoding)
+else:
+    seq_df["seg_label"] = 0
+
+if input_params.test:
+    seq_df = seq_df[seq_df.split == "test"]
+else:
+    seq_df = seq_df[seq_df.split != "test"]
+
+##############
+### device ###
+##############
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("\nCUDA device: GPU\n")
+else:
+    device = torch.device("cpu")
+    print("\nCUDA device: CPU\n")
+
+gc.collect()
+torch.cuda.empty_cache()
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
+
+
+#############################################
+### define dataloader & collate functions ###
+#############################################
+
 
 class SeqDataset(Dataset):
     def __init__(self, seq_df, transform, max_augm_shift=0, mode="train"):
@@ -151,23 +199,20 @@ class SeqDataset(Dataset):
     def __len__(self):
         return len(self.seq_df)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx):  # returns two haplotype sequences (father and mother)
         if self.fasta:
             seq = self.fasta.fetch(self.seq_df.iloc[idx].seq_name).upper()
         else:
             seq = self.seq_df.iloc[idx].seq.upper()
 
-        shift = np.random.randint(
-            self.max_augm_shift + 1
-        )  # random shift at training, must be chunk_size-input_params.seq_len
+        # random shift at training, must be chunk_size-input_params.seq_len
+        shift = np.random.randint(self.max_augm_shift + 1)
 
         seq = seq[shift : shift + input_params.seq_len]  # shift the sequence and limit its size
-
         seg_label = self.seq_df.iloc[idx].seg_label  # label for segment-aware training
 
-        # create sequences for whole genotype (adjustments from Sarah)
-        seq1 = seq.replace("-", "").replace("B", "A").replace("F", "A").replace("M", "R") # father sequence
-        seq2 = seq.replace("-", "").replace("B", "A").replace("M", "A").replace("F", "R") # mother sequence
+        seq1 = seq.replace("-", "").replace("B", "A").replace("F", "A").replace("M", "R")  # father
+        seq2 = seq.replace("-", "").replace("B", "A").replace("M", "A").replace("F", "R")  # mother
 
         masked_sequence1, target_labels_masked1, target_labels1, _, _ = self.transform(seq1)
         masked_sequence2, target_labels_masked2, target_labels2, _, _ = self.transform(seq2)
@@ -178,118 +223,63 @@ class SeqDataset(Dataset):
 
         target_labels_masked = torch.vstack((target_labels_masked1, target_labels_masked2))
         target_labels = torch.vstack((target_labels1, target_labels2))
-        seq = (seq1, seq2)
-        return masked_sequence, target_labels_masked, target_labels, seq
 
-        # if input_params.masking == 'stratified_maf' and not input_params.test:
-        #    #select mask for the sequence depending on sequence coordinates w.r.t. contig
-        #    seg_name = self.seq_df.iloc[idx].seg_name
-        #    seq_mask = meta.loc[seg_name].MASK.values
-        #    masked_sequence, target_labels_masked, target_labels, _, _ = self.transform(seq, mask = seq_mask)
-        # else:
-        #    masked_sequence, target_labels_masked, target_labels, _, _ = self.transform(seq)
+        return masked_sequence, target_labels_masked, target_labels, (seq1, seq2)
 
     def close(self):
         self.fasta.close()
 
 
-def collate_fn(batch):  # by Sarah
+def collate_fn(data):  # Sarah's collate fn
     # masked sequence
-    masked_sequence = [x[0][0] for x in batch]
+    masked_sequence = [x[0][0] for x in data]
     masked_sequence = [torch.stack(torch.split(d, 3)) for d in masked_sequence]
     masked_sequence = torch.concat(masked_sequence)
     # seg labels
-    seg_labels = [x[0][1] for x in batch]
+    seg_labels = [x[0][1] for x in data]
     seg_labels = torch.concat(seg_labels).flatten()
     # target labels masked
-    target_labels_masked = [x[1] for x in batch]
+    target_labels_masked = [x[1] for x in data]
     target_labels_masked = torch.concat(target_labels_masked)
     # target labels
-    target_labels = [x[2] for x in batch]
+    target_labels = [x[2] for x in data]
     target_labels = torch.concat(target_labels)
     # seq
-    seqs = [x[3] for x in batch]
+    seqs = [x[3] for x in data]
     seqs = tuple(chain.from_iterable(seqs))
     return (masked_sequence, seg_labels), target_labels_masked, target_labels, seqs
 
 
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print("\nCUDA device: GPU\n")
-else:
-    device = torch.device("cpu")
-    print("\nCUDA device: CPU\n")
-    # raise Exception('CUDA is not found')
+def collate_fn_get_embeddings(data):  # Sarah's collate fn for testing with get_ebeddings = True
+    # masked sequence
+    masked_sequence = [x[0][0] for x in data]
+    # 50 = mask_stride of sequence_encoders.RollingMasker
+    masked_sequence = [torch.stack(torch.split(d, split_size_or_sections=50, dim=0)) for d in masked_sequence]
+    masked_sequence = torch.concat(masked_sequence)
+    # seg labels
+    seg_labels = [x[0][1] for x in data]
+    seg_labels = torch.concat(seg_labels).flatten()
+    # target labels masked
+    target_labels_masked = [x[1] for x in data]
+    target_labels_masked = [torch.stack(torch.split(d, split_size_or_sections=50, dim=0)) for d in target_labels_masked]
+    target_labels_masked = torch.concat(target_labels_masked)
+    # target labels
+    target_labels = [x[2] for x in data]
+    target_labels = [torch.stack(torch.split(d, split_size_or_sections=50, dim=0)) for d in target_labels]
+    target_labels = torch.concat(target_labels)
+    # seq
+    seqs = [x[3] for x in data]
+    seqs = tuple(chain.from_iterable(seqs))
+    return (masked_sequence, seg_labels), target_labels_masked, target_labels, seqs
 
 
-gc.collect()
-torch.cuda.empty_cache()
-
-import os
-
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
-
-if input_params.dataset.endswith(".fa"):
-    seq_df = pd.read_csv(input_params.dataset + ".fai", header=None, sep="\t", usecols=[0], names=["seq_name"])
-elif input_params.dataset.endswith(".parquet"):
-    seq_df = pd.read_parquet(input_params.dataset).reset_index()
-
-seq_df[["split", "sample_id", "seg_name"]] = seq_df["seq_name"].str.split(":", expand=True)
-
-
-# for each contig, mark first and lask chunks
-# seq_df[['split','sample_id','chrom','pos']] =  seq_df['seq_name'].str.split(':',expand=True)
-
-# seq_df.pos = seq_df.pos.astype(int)
-
-# df = seq_df.sort_values(by=['sample_id','chrom','pos']).copy()
-# df['last_chunk'] = ~df.duplicated(subset=['sample_id','chrom'],keep='last')
-# df['first_chunk'] = ~df.duplicated(subset=['sample_id','chrom'],keep='first')
-
-# seq_df = seq_df.merge(df[['seq_name','first_chunk','last_chunk']])
-
-
-# from sklearn.model_selection import train_test_split
-#
-# meta = pd.read_csv(os.path.dirname(input_params.dataset) + '/meta.csv.gz')[['GENE_ID','AF']]
-# meta['MAF'] = meta.AF.apply(lambda x: min(x,1-x)) #minor allele frequency
-#
-# meta['MAF_bin'] = np.digitize(meta.MAF, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5], right=True) #bin indices for MAF
-#
-# meta.set_index('GENE_ID', inplace=True)
-#
-# def get_random_mask():
-#    '''
-#    randomly mask positions at mask_rate
-#    stratify by MAF distribution
-#    '''
-#    mask = np.zeros(len(meta.MAF_bin))
-#    masked_idx, _ = train_test_split(np.arange(len(meta.MAF_bin)), train_size=input_params.mask_rate,
-#                    shuffle=True, stratify=meta.MAF_bin)
-#    mask[masked_idx] = 1
-#    meta['MASK'] = mask
-#
-#    return meta
-
-if not input_params.agnostic:
-    # for segment-aware model, assign a label to each segment
-    seg_name = seq_df.seq_name.apply(lambda x: ":".join(x.split(":")[2:]))
-    segment_encoding = seg_name.drop_duplicates().reset_index(drop=True)
-    segment_encoding = {seg_name: idx for idx, seg_name in segment_encoding.items()}
-    seq_df["seg_label"] = seg_name.map(segment_encoding)
-else:
-    seq_df["seg_label"] = 0
-
-
-if input_params.test:
-    seq_df = seq_df[seq_df.split == "test"]
-else:
-    seq_df = seq_df[seq_df.split != "test"]
+#########################
+### create dataloader ###
+#########################
 
 test_df = None
 
-if not input_params.test:
-    # Train and Validate
+if not input_params.test:  # Train and Validate
     seq_transform = sequence_encoders.SequenceDataEncoder(
         seq_len=input_params.seq_len,
         total_len=input_params.seq_len,
@@ -297,61 +287,55 @@ if not input_params.test:
         split_mask=input_params.split_mask,
     )
 
-    # N_train = int(len(seq_df)*(1-input_params.val_fraction))
-
     if input_params.fold is not None:
         samples = seq_df.sample_id.unique()
         val_samples = samples[input_params.fold :: input_params.Nfolds]
-
         train_df = seq_df[~seq_df.sample_id.isin(val_samples)]
         test_df = seq_df[seq_df.sample_id.isin(val_samples)]
-
         test_dataset = SeqDataset(test_df, transform=seq_transform, mode="eval")
         test_dataloader = DataLoader(
             dataset=test_dataset, batch_size=input_params.batch_size, num_workers=0, collate_fn=collate_fn, shuffle=False
         )
-
     else:
         train_df = seq_df
 
-        # train_df = seq_df[seq_df.split=='train']
-        # test_df = seq_df[seq_df.split=='val']
-
     N_train = len(train_df)
-
     train_fold = np.repeat(list(range(input_params.train_splits)), repeats=N_train // input_params.train_splits + 1)
     train_df["train_fold"] = train_fold[:N_train]
-
+    # create training dataset & dataloader
     train_dataset = SeqDataset(train_df, transform=seq_transform, mode="train")
     train_dataloader = DataLoader(
         dataset=train_dataset, batch_size=input_params.batch_size, num_workers=2, collate_fn=collate_fn, shuffle=False
     )
 
-
-elif input_params.get_embeddings:
+elif input_params.get_embeddings:  # Test (get_embeddings = True)
     if input_params.mask_at_test:
         seq_transform = sequence_encoders.RollingMasker(mask_stride=50, frame=0)
     else:
         seq_transform = sequence_encoders.PlainOneHot(frame=0, padding="none")
-
+    # create test dataset & dataloader
     test_dataset = SeqDataset(seq_df, transform=seq_transform, mode="eval")
+    test_dataloader = DataLoader(
+        dataset=test_dataset, batch_size=1, num_workers=1, collate_fn=collate_fn_get_embeddings, shuffle=False
+    )
 
-    test_dataloader = DataLoader(dataset=test_dataset, batch_size=1, num_workers=1, collate_fn=collate_fn, shuffle=False)
-
-else:
-    # Test
+else:  # Test (get_embeddings = False)
     seq_transform = sequence_encoders.SequenceDataEncoder(
         seq_len=input_params.seq_len,
         total_len=input_params.seq_len,
         mask_rate=input_params.mask_rate,
         split_mask=input_params.split_mask,
     )
-
+    # create test dataset & dataloader
     test_dataset = SeqDataset(seq_df, transform=seq_transform, mode="eval")
     test_dataloader = DataLoader(
         dataset=test_dataset, batch_size=input_params.batch_size, num_workers=2, collate_fn=collate_fn, shuffle=False
     )
 
+
+####################
+### define model ###
+####################
 
 seg_encoder = SpecAdd(embed=True, encoder="label", Nsegments=seq_df.seg_label.nunique(), d_model=input_params.d_model)
 
@@ -364,13 +348,15 @@ model = DSSResNetEmb(
     embed_before=True,
     species_encoder=seg_encoder,
 )
-
 model = model.to(device)
 
 model_params = [p for p in model.parameters() if p.requires_grad]
-
 optimizer = torch.optim.Adam(model_params, lr=input_params.learning_rate, weight_decay=input_params.weight_decay)
 
+
+#######################
+### set up training ###
+#######################
 
 last_epoch = 0
 
@@ -393,14 +379,38 @@ weights_dir = os.path.join(input_params.output_dir, "weights")  # dir to save mo
 if input_params.save_at:
     os.makedirs(weights_dir, exist_ok=True)
 
-# lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
-#        milestones=input_params.lr_sch_milestones, gamma=input_params.lr_sch_gamma, verbose=False)
-
 
 def metrics_to_str(metrics):
     loss, accuracy, masked_acc, masked_recall, masked_IQS = metrics
     return f'loss: {loss:.4}, acc: {accuracy:.4}, masked acc: {masked_acc:.4}, {misc.print_class_recall(masked_recall, "masked recall: ")}, masked IQS: {masked_IQS:.4}'
 
+
+from sklearn.model_selection import train_test_split
+
+meta = pd.read_csv(os.path.dirname(input_params.dataset) + "/meta.csv.gz")[["GENE_ID", "AF"]]
+meta["MAF"] = meta.AF.apply(lambda x: min(x, 1 - x))  # minor allele frequency
+meta["MAF_bin"] = np.digitize(meta.MAF, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5], right=True)  # bin indices for MAF
+meta.set_index("GENE_ID", inplace=True)
+
+
+def get_random_mask():
+    """
+    randomly mask positions at mask_rate
+    stratify by MAF distribution
+    """
+    mask = np.zeros(len(meta.MAF_bin))
+    masked_idx, _ = train_test_split(
+        np.arange(len(meta.MAF_bin)), train_size=input_params.mask_rate, shuffle=True, stratify=meta.MAF_bin
+    )
+    mask[masked_idx] = 1
+    meta["MASK"] = mask
+
+    return meta
+
+
+####################################
+### actual training/testing loop ###
+####################################
 
 from helpers.misc import print  # print function that displays time
 
@@ -430,13 +440,11 @@ if not input_params.test:
             val_metrics, *_ = train_eval.model_eval(model, optimizer, test_dataloader, device, silent=1)
 
             print(f"epoch {epoch} - validation, {metrics_to_str(val_metrics)}")
-
-        # lr_scheduler.step()
 else:
     print(f"EPOCH {last_epoch}: Test/Inference...")
 
     test_metrics, test_embeddings, motif_probas = train_eval.model_eval(
-        model, optimizer, test_dataloader, device, get_embeddings=input_params.get_embeddings, silent=0
+        model, test_dataloader, device, get_embeddings=input_params.get_embeddings, diploid=True, silent=False
     )
 
     print(f"epoch {last_epoch} - test, {metrics_to_str(test_metrics)}")
@@ -445,10 +453,7 @@ else:
         os.makedirs(input_params.output_dir, exist_ok=True)
 
         with open(input_params.output_dir + "/embeddings.pickle", "wb") as f:
-            # test_embeddings = np.vstack(test_embeddings)
-            # np.save(f,test_embeddings)
             pickle.dump(test_embeddings, f)
-            # pickle.dump(seq_df.seq_name.tolist(),f)
 
 print()
 print(f"peak GPU memory allocation: {round(torch.cuda.max_memory_allocated(device)/1024/1024)} Mb")
